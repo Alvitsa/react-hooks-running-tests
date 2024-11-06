4c524e89e45f31b3a50b9b0fd012e5ac6e92bb92
+ <+>
2024-11-06 11:55:09 +0300

    test files

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 function Article() {
-  return (
-    <div>
-      {/* please pass this test */}
-    </div>
-  );
+  return  <div> please pass this test</div>;
 }
 
 export default Article;
+
+// src/__tests__/Article.test.js
+test("displays the text 'please pass this test'", () => {
+  render(<Article />);
+
+  // add this line
+  screen.debug();
+
+  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+});
